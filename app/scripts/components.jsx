d95ec6097e400f9a93691e816058d28acf310cba
stahl1 <jon.stahlstrom@gmail.com>
2015-04-13 16:09:46 +0300

    initial commit

---
+++ app/scripts/components.jsx
@@ -0,0 +1,161 @@
+var Route = React.createClass({
+    openRouteOnMap: function() {
+      openRouteMap(this.props.route.route_id);
+
+    },
+    render: function() {
+        var pattern = _.find(DATA.patterns,{route_id:this.props.route.route_id});
+        var routeStartText = _.find(
+              DATA.stops,
+              { stop_id:pattern.stops[0].stop_id}
+        ).stop_name;
+        var routeStopText = _.find(
+              DATA.stops,
+              { stop_id:pattern.stops[pattern.stops.length-1].stop_id}
+        ).stop_name;
+        return (
+          <div className='route' onClick={this.openRouteOnMap}>
+            {this.props.route.route_short_name} {routeStartText}-{routeStopText}
+          </div>
+        );
+    }
+});
+var RoutesList = React.createClass({
+    render: function() {
+        var routes = [];
+        routes = this.props.data.routes.map(function(route){
+          return <Route route={route} key={route.route_id} />;
+        });
+        return (
+          <div className='routes-list'>
+            {routes}
+          </div>
+        );
+    }
+});
+var RouteCategoryList = React.createClass({
+    expandThis: function() {
+        this.props.expandCategory(this.props.categoryName);
+    },
+    render: function() {
+        var routesList;
+        if(this.props.expanded){
+          routesList = <RoutesList  data={this.props.data}/>;
+        } else {
+          routesList = '';
+        }
+        return (
+          <div className='vantaa-routes routes accordion' data-expanded={this.props.expanded}>
+            <button className='expand-routes-list' onClick={this.expandThis}>Kehärata ja Vantaan bussilinjasto</button>
+            {routesList}
+          </div>
+        );
+    }
+});
+var AllRoutesList = React.createClass({
+    getInitialState: function() {
+       return {
+           addressSearchText: '',
+           openRouteCategory: ''
+       };
+    },
+    expandCategory:function(categoryName) {
+      this.setState({openRouteCategory:'vantaa'});
+    },
+    render: function() {
+        return (
+          <div className='routes-group'>
+            <RouteCategoryList data={DATA} routeCategory='vantaa' expanded={(this.state.openRouteCategory==='vantaa')?true:false} expandCategory={this.expandCategory}/>
+          </div>
+        );
+    }
+});
+var RouteSearchBox = React.createClass({
+    getInitialState: function() {
+       return {
+           suggestions: [],
+           suggestionsForFrom: false,
+           suggestionsForTo: false
+       };
+    },
+    showSuggestions:function(suggestions) {
+      this.setState({suggestions:suggestions});
+
+    },
+    search: function(e) {
+      if(e.target.name==='from-place'){
+        this.setState({suggestionsForFrom:true,suggestionsForTo:false,suggestionsForLine:false});
+
+      } else if(e.target.name==='to-place'){
+        this.setState({suggestionsForFrom:false,suggestionsForTo:true,suggestionsForLine:false});
+      }
+      $.get('http://matka.hsl.fi/otp/otp/routers/default/geocode',
+            {query:e.target.value}).then(this.showSuggestions);
+    },
+    searchCurrentlines: function(e) {
+      if(e.target.value!=='') {
+        this.setState({suggestionsForFrom:false, suggestionsForTo:false, suggestionsForLine:true});
+        var matches = _.where(replacementLines,{oldLines:[e.target.value]});
+        this.showSuggestions(matches);
+        var newRoutes =  _.map(matches, function(match) {
+
+          return getRoutePattern(_.find(DATA.routes,{route_short_name:match.newLine}).route_id);
+        });
+        return showRoutesOnMap(newRoutes);
+      } else {
+        return clearRoutesOnMap();
+      }
+    },
+    render: function() {
+
+      var suggestions;
+      if (this.state.suggestions && !this.state.suggestionsForLine) {
+        suggestionsitems = this.state.suggestions.map(function(suggestion,index){
+          return <li key={suggestion.id}>{suggestion.description}</li>;
+        });
+        suggestions = <ul>{suggestionsitems}</ul>;
+      } else if(this.state.suggestions && this.state.suggestionsForLine) {
+        suggestionsitems = this.state.suggestions.map(function(suggestion,index){
+          return <li key={suggestion.newLine}>{suggestion.newLine}</li>;
+        });
+        suggestions = <div className='suggestions'>
+                        <h3>Korvaavat linjat:</h3>
+                        <ul>{suggestionsitems}</ul>;
+                      </div>;
+      } else {
+        var suggestions = '';
+      }
+
+        return (
+          <div className='route-search-container'>
+            <form name='route-search-form'>
+              <div className='form-group'>
+                <input type='text' className='autocomplete typeahead' onChange={this.search} name='from-place' placeholder='mistä' />
+                {this.state.suggestionsForFrom ? suggestions : ''}
+              </div>
+              <div className='form-group'>
+                <input type='text' className='autocomplete typeahead' onChange={this.search} name='to-place' placeholder='mihin' />
+                {this.state.suggestionsForTo ? suggestions : ''}
+              </div>
+              <h2>Tai hae nykyisellä linjanumerolla:</h2>
+              <div className='form-group'>
+                <input type='text' className='autocomplete typeahead' onChange={this.searchCurrentlines} name='current-linenr' placeholder='nykyinen linjanumero' />
+                {this.state.suggestionsForLine ? suggestions : ''}
+              </div>
+              <button type='submit'>Hae reitti</button>
+            </form>
+          </div>
+        );
+    }
+});
+var LeftSidebar = React.createClass({
+  render: function(){
+    return (
+      <div>
+        <RouteSearchBox />
+        <AllRoutesList data={DATA} />
+      </div>
+    );
+  }
+});
+React.render(<LeftSidebar data={DATA} />, document.getElementById('sidebar'));
---
+++ app/scripts/main.js
@@ -0,0 +1,127 @@
+var initialLayers = L.layerGroup();
+var showLayer = L.layerGroup();
+var map;
+var transitive;
+DATA.allPatterns = DATA.patterns;
+function getRoutePattern(routeId) {
+  var routePattern = _.find(DATA.allPatterns, {route_id:routeId});
+  return routePattern;
+}
+function showRoutesOnMap(routes) {
+  var maxRouteLat = 0;
+  var maxRouteLon = 0;
+  var minRouteLat = 0;
+  var minRouteLon = 0;
+  showLayer.clearLayers();
+  for(var i=0; i<routes.length; i++) {
+    showLayer.addLayer(routes[i].polyline);
+    showLayer.addLayer(routes[i].startMarker);
+    showLayer.addLayer(routes[i].stopMarker);
+  }
+  DATA.patterns = [];
+  DATA.allPatterns.map(function(pattern, index) {
+    for (var i = 0; i < routes.length; i++) {
+      if (pattern.route_id === routes[i].route_id) {
+        pattern.render = true;
+        DATA.patterns.push(pattern);
+      }
+
+    }
+
+  });
+  DATA.patterns[0].render = false;
+  transitive.updateData(DATA);
+
+  //map.fitBounds([route.stopLatLongs[0],route.stopLatLongs[(route.stopLatLongs.length-1)]]);
+}
+function clearRoutesOnMap(){
+  showLayer.clearLayers();
+  DATA.patterns = [];
+  transitive.updateData(DATA);
+}
+function startMap() {
+
+    var stopLatLongs=[];
+    var stopStartMarkers=[];
+    var polylines = [];
+    var markers = [];
+    var oneway = [];
+    var marker;
+
+   map = L.map('map',{
+    inertia: false,
+    zoomAnimation: false
+  });
+  L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
+  			maxZoom: 18,
+  			attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+  				'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+  				'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+  			id: 'examples.map-i875mjb7'
+  }).addTo(map);
+  for(var i=0; i<= DATA.patterns.length-1; i++){
+    stopLatLongs = [];
+    stopLatLongs = DATA.patterns[i].stops.map(function(stop,index){
+      var stopData = _.where(DATA.stops,{'stop_id':stop.stop_id})[0];
+      var arr = [stopData.stop_lat,stopData.stop_lon];
+      if(index===0) {
+        DATA.patterns[i].startMarker = L.marker(arr,{title:stopData.stop_name}).bindPopup(stopData.stop_name);
+      } else if(index===DATA.patterns[i].stops.length-1) {
+        DATA.patterns[i].stopMarker = L.marker(arr,{title:stopData.stop_name}).bindPopup(stopData.stop_name);
+      }
+      return arr;
+    });
+    DATA.patterns[i].stopLatLongs = stopLatLongs;
+    DATA.patterns[i].polyline = L.polyline(stopLatLongs, {color: 'blue'});
+  }
+  // initialLayers.addTo(map);
+  // create the Transitive instance
+  // var journeys = [];
+  // journeys = DATA.patterns.map(function(pattern) {
+  //
+  //   return {
+  //     "journey_id": "journey_pattern_"+pattern.pattern_id,
+  //     "journey_name": "journey for pattern:"+pattern.pattern_id,
+  //     "segments": [{
+  //       "type": "TRANSIT",
+  //       "pattern_id": pattern.pattern_id,
+  //       "from_stop_index": 0,
+  //       "to_stop_index": (pattern.stops.length-1)
+  //     }]
+  //     };
+  // });
+  // DATA.journeys = journeys.slice(0,50);
+  transitive = new Transitive({
+     data: DATA,
+     autoResize: false,
+     zoomEnabled: false
+  });
+  map.addLayer(new L.TransitiveLayer(transitive));
+  //map.addLayer(showLayer);
+  map.setView([60.287481, 24.996849], 11);
+  // O/D points
+  // var od = {
+  //   from: {
+  //     name: 'Home',
+  //     lat: 60.187578,
+  //     lon: 24.960113
+  //   },
+  //   to: {
+  //     name: 'Work',
+  //     lat: 60.199012,
+  //     lon: 24.940372
+  //   }
+  // };
+
+  // Create new instance
+  // var profiler = new OtpProfiler({
+  //   host: 'http://matka.hsl.fi/otp/routers/default',
+  //   limit: 3 // limit the number of options to profile, defaults to 3
+  // });
+  // profiler.profile(od, function(err, profile) {
+  //   console.log(profile); // Outputs the profile of the journey
+  // });
+}
+$(document).ready(function() {
+  startMap();
+});
